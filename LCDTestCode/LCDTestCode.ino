f8863951ea75f710123489d8c23d63917f871bde
CLFalkner <47128769+CLFalkner@users.noreply.github.com>
2021-12-10 18:06:11 -0500

    Add comments

--- LCDTestCode/LCDTestCode.ino
+++ LCDTestCode/LCDTestCode.ino
@@ -1,17 +1,18 @@
 //www.diyusthad.com
 #include <LiquidCrystal.h>
 
+// declare the pins for the LCD
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);    // initialize the LCD with the declared pins
 
 void setup() {
-  lcd.begin(16, 2);
+  lcd.begin(16, 2);                     // tell the LCD the dimensions of the display (count the white boxes)
   lcd.print("hello, world");
   
 }
 
 void loop() {
   // run timer
-  lcd.setCursor(0,1);
-  lcd.print(millis() / 1000);
+  lcd.setCursor(0,1);                   // choose the line and slot to display the timer in
+  lcd.print(millis() / 1000);           // display the timer
 }
