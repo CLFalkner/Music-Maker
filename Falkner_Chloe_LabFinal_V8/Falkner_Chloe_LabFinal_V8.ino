c36a43f4d4d0069e4594d029c582769ac4663b16
CLFalkner <47128769+CLFalkner@users.noreply.github.com>
2021-12-10 19:24:51 -0500

    Add comments

--- Falkner_Chloe_LabFinal_V8/Falkner_Chloe_LabFinal_V8.ino
+++ Falkner_Chloe_LabFinal_V8/Falkner_Chloe_LabFinal_V8.ino
@@ -20,24 +20,26 @@
    6. Display octave and note information on LCD (not working)
    6.a. Tutorial: https://create.arduino.cc/projecthub/najad/interfacing-lcd1602-with-arduino-764ec4
    7. Troubleshoot
-   7.a. The LCD and IrReceiver are working properly, but not the speaker.
+   7.a. The LCD and speaker are working properly, but not the Ir Receiver.
+   8. The LCD and IrReceiver are working properly, but not the speaker.
 */
 
-#include <IRremote.hpp>
-#include "pitches.h"
-#include <LiquidCrystal.h>
+// include relevant libraries
+#include <IRremote.hpp>                             // for the IR Receiver
+#include "pitches.h"                                // contains the note frequencies
+#include <LiquidCrystal.h>                          // for the LCD
 
 // create global variables
 int speaker = 8;                                    // pin to control active speaker
-int RECV_PIN = 10;
-int octave = 3;
-int note = 0;
+int RECV_PIN = 10;                                  // IR Receiver pin
+int octave = 3;                                     // keep track of the current octave
+int note = 0;                                       // keep track of the current note
 
 // set up LCD
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// button codes
+// button codes (you will need to update these)
 //#define power       0xFFA25D
 #define volumeUp    0xB946FF00
 //#define funcStop    0xFFE21D
@@ -61,13 +63,14 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 //#define nine        0xFF52AD
 
 // array to hold octaves
-int octaves[7][8] = {{NOTE_B0, NOTE_C1, NOTE_D1, NOTE_E1, NOTE_F1, NOTE_G1, NOTE_A1, NOTE_B1},
-  {NOTE_B1, NOTE_C2, NOTE_D2, NOTE_E2, NOTE_F2, NOTE_G2, NOTE_A2, NOTE_B2},
-  {NOTE_B2, NOTE_C3, NOTE_D3, NOTE_E3, NOTE_F3, NOTE_G3, NOTE_A3, NOTE_B3},
-  {NOTE_B3, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4},
-  {NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5},
-  {NOTE_B5, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6},
-  {NOTE_B6, NOTE_C7, NOTE_D7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_A7, NOTE_B7}
+// while the LCD displays the octaves as 1-7, the array stores them as 0-6
+int octaves[7][8] = {{NOTE_B0, NOTE_C1, NOTE_D1, NOTE_E1, NOTE_F1, NOTE_G1, NOTE_A1, NOTE_B1},  // octave 0
+  {NOTE_B1, NOTE_C2, NOTE_D2, NOTE_E2, NOTE_F2, NOTE_G2, NOTE_A2, NOTE_B2},                     // octave 1
+  {NOTE_B2, NOTE_C3, NOTE_D3, NOTE_E3, NOTE_F3, NOTE_G3, NOTE_A3, NOTE_B3},                     // octave 2
+  {NOTE_B3, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4},                     // octave 3
+  {NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5},                     // octave 4
+  {NOTE_B5, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6},                     // octave 5
+  {NOTE_B6, NOTE_C7, NOTE_D7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_A7, NOTE_B7}                      // octave 6
 };
 
 void setup() {
@@ -90,69 +93,69 @@ void loop() {
   {
     //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
     //IrReceiver.printIRResultShort(&Serial);
-    switch (IrReceiver.decodedIRData.decodedRawData) {                           // which button is pressed
+    switch (IrReceiver.decodedIRData.decodedRawData) {  // which button is pressed
       case one:
         noTone(speaker);
         note = 0;                                      // low B
-        Serial.println("B");
-        displayNote("B");
-        delay(10);
+        Serial.println("B");                           // print B to Serial Monitor
+        displayNote("B");                              // print B to LCD
+        delay(10);                                     // delay implemented during troubleshooting. Not sure if it helped
         break;
       case two:
         noTone(speaker);
         note = 1;                                      // C
-        displayNote("C");
-        Serial.println("C");
-        delay(10);
+        displayNote("C");                              // print C to Serial Monitior
+        Serial.println("C");                           // print C to LCD
+        delay(10);                                     // delay implemented during troubleshooting. Not sure if it helped
         break;
       case three:
         note = 2;                                      // D
-        displayNote("D");
-        Serial.println("D");
-        delay(10);
+        displayNote("D");                              // print D to Serial Monitior
+        Serial.println("D");                           // print D to LCD
+        delay(10);                                     // delay implemented during troubleshooting. Not sure if it helped
         break;
       case four:
         note = 3;                                      // E
-        displayNote("E");
-        Serial.println("E");
-        delay(10);
+        displayNote("E");                              // print E to Serial Monitior
+        Serial.println("E");                           // print E to LCD
+        delay(10);                                     // delay implemented during troubleshooting. Not sure if it helped
         break;
       case five:
         note = 4;                                      // F
-        displayNote("F");
-        Serial.println("F");
+        displayNote("F");                              // print F to Serial Monitior
+        Serial.println("F");                           // print F to LCD
         break;
       case six:
         note = 5;                                      // G
-        displayNote("G");
-        Serial.println("G");
+        displayNote("G");                              // print G to Serial Monitior
+        Serial.println("G");                           // print G to LCD
         break;
       case seven:
         note = 6;                                      // A
-        displayNote("A");
-        Serial.println("A");
+        displayNote("A");                              // print A to Serial Monitior
+        Serial.println("A");                           // print A to LCD
         break;
       case eight:
         note = 7;                                      // high B
-        displayNote("B");
-        Serial.println("B");
+        displayNote("B");                              // print B to LCD
+        Serial.println("B");                           // print B to Serial Monitor
         break;
-      case volumeUp:                                   // increase octave
-        octave++;
-        if (octave > 6) {                              // loop octaves
+      case volumeUp:
+        octave++;                                      // increase octave
+        if (octave > 6) {                              // loop octaves from high to low
           octave = 0;
         }
-        displayOctave();
+        displayOctave();                               // print octave number to LCD
         break;
-      case volumeDown:                                 // decrease octave
-        octave--;
-        if (octave < 0) {                              // loop octaves
+      case volumeDown: 
+        octave--;                                      // decrease octave
+        if (octave < 0) {                              // loop octaves from low to high
           octave = 6;
         }
-        displayOctave();
+        displayOctave();                               // print octave number to LCD
         break;
       default:
-        //noTone(speaker);
+        //noTone(speaker);                              // program doesn't like this line
         break;
     }
 
@@ -169,17 +172,20 @@ void loop() {
 
 }
 
+// display basic text
 void displayText() {
   lcd.print("Octave: ");
   lcd.setCursor(0, 1);
   lcd.print("Note: ");
 }
 
+// update the octave displayed
 void displayOctave() {
   lcd.setCursor(8, 0);
   lcd.print(octave + 1);
 }
 
+// update the note displayed
 void displayNote(String note_) {
   lcd.setCursor(8, 1);
   lcd.print(note_);
